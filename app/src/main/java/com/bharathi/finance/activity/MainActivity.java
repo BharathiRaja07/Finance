cfe085fbcc82f15cc5f5f592900ea91790bf6ce3
prashansha01 <prashanshaav@gmail.com>
2019-12-23 17:16:33 +0530

    offers section

--- app/src/main/java/com/bharathi/finance/activity/MainActivity.java
+++ app/src/main/java/com/bharathi/finance/activity/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.tempButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CreateProfileActivity.class);
+                Intent intent = new Intent(MainActivity.this, MainPanel.class);
                 startActivity(intent);
             }
         });
